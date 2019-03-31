b21294ae0e6458e2eb71ce433c2cbe3324690c17
Cai Yang <caih.yang@gmail.com>
2019-03-31 16:46:13 -0400

    additional features

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ let player = document.getElementsByClassName('player');
 let filled1 = [];
 let filled2 = [];
 let turn = 0;
+let aWinner = false;
 let reload = document.getElementById('gameover');
 
 for (let i = 0; i < box.length; i++) {
   box[i].addEventListener('click', () => {
-
 
     if (isFilled(i)) {
       if (turn == 0) {
@@ -17,7 +17,7 @@ for (let i = 0; i < box.length; i++) {
         player[0].classList.remove('highlight');
         player[1].classList.add('highlight');
         filled1.push(i);
-        isThereAWinner(filled1);
+        isThereAWinner(filled1,"Player 1");
         turn = 1;
       } else {
         box[i].classList.add('x');
@@ -25,10 +25,15 @@ for (let i = 0; i < box.length; i++) {
         player[1].classList.remove('highlight');
         filled2.push(i);
         turn = 0;
-        isThereAWinner(filled2);
+        isThereAWinner(filled2,"Player 2");
       }
     } else {
       alert('Already filled');
+    }
+
+    if(filled1.length == 5 && aWinner == false){
+      alert(`IT'S A TIE!!!`);
+      pop();
     }
 
   });
@@ -42,41 +47,42 @@ const isFilled = (element) => {
   }
 }
 
-const isThereAWinner = (filled) => {
+const isThereAWinner = (filled, player) => {
   if (filled.includes(0) && filled.includes(1) && filled.includes(2)) {
-    alert(`GameOver!!!`);
+    alert(`${player} wins!!!`);
     pop();
   }
   if (filled.includes(0) && filled.includes(4) && filled.includes(8)) {
-    alert(`GameOver!!!`);
+    alert(`${player} wins!!!`);
     pop();
   }
   if (filled.includes(0) && filled.includes(3) && filled.includes(6)) {
-    alert(`GameOver!!!`);
+    alert(`${player} wins!!!`);
     pop();
   }
   if (filled.includes(3) && filled.includes(4) && filled.includes(5)) {
-    alert(`GameOver!!!`);
+    alert(`${player} wins!!!`);
     pop();
   }
   if (filled.includes(6) && filled.includes(7) && filled.includes(8)) {
-    alert(`GameOver!!!`);
+    alert(`${player} wins!!!`);
     pop();
   }
   if (filled.includes(6) && filled.includes(4) && filled.includes(2)) {
-    alert(`GameOver!!!`);
+    alert(`${player} wins!!!`);
     pop();
   }
   if (filled.includes(2) && filled.includes(5) && filled.includes(8)) {
-    alert(`GameOver!!!`);
+    alert(`${player} wins!!!`);
     pop();
   }
   if (filled.includes(1) && filled.includes(4) && filled.includes(7)) {
-    alert(`GameOver!!!`);
+    alert(`${player} wins!!!`);
     pop();
   }
 }
 
 const pop = () => {
   reload.style.display = 'block';
+  aWinner = true;
 }
